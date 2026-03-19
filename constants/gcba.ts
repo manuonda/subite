aef3638809d95bb16ac4711044abaf979770fd9d
david garcia <david.garcia@grupomost.com>
2026-03-19 14:43:38 -0300

    Enhance Mapa component with improved marker clustering and optimize performance; update styles for better responsiveness across devices.

---
+++ constants/gcba.ts
@@ -0,0 +1,6 @@
+//BASE URL para la API del GCBA
+export const GCBA_BASE = "https://apitransporte.buenosaires.gob.ar";
+
+//CLIENT ID y CLIENT SECRET para la API del GCBA
+export const GCBA_CLIENT_ID = process.env.GCBA_CLIENT_ID;
+export const GCBA_CLIENT_SECRET = process.env.GCBA_CLIENT_SECRET;
---
+++ constants/geo.ts
@@ -0,0 +1,2 @@
+//Coordenadas de Buenos Aires
+export const BA_CENTER = { lat: -34.6037, lng: -58.3816 };
